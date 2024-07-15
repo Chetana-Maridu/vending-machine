a4c21cb2788b9c7a91ae007683753fbfa945f3e4
chetana sree <mariduchetana@gmail.com>
2024-07-15 13:54:07 +0530

    Add files via upload

---
+++ vending machine/vending machine.c
@@ -0,0 +1,316 @@
+#include<stdio.h>
+int main()
+{
+    int choice,cost,amount,correctamount,change;
+    printf("************WELCOME TO THE VENDING MACHINE APPLICATION ************\n");
+    printf("Main Menu\n");
+    printf("1---Lays\n");
+    printf("2---Kurkure\n");
+    printf("3---Pepsi\n");
+    printf("4---Thums Up\n");
+    printf("5---Sprite\n");
+    printf("6---Mirinda\n");
+    printf("7---Mountain Dew\n");
+    printf("8---Maaza\n");
+    printf("9---Slice\n");
+    printf("10---Lipton Green Tea\n");
+    printf("11---Bisleri\n");
+    printf("12---Kinley\n");
+    printf("13---Bailey\n");
+    printf("14---Tropicana(Mango)\n");
+    printf("15---Tropicana(Mixed fruit)\n");
+    printf("16---Tropicana(Apple)\n");
+    printf("17---Tropicana(Orange)\n");
+    printf("18---Dairy milk\n");
+    printf("19---Mars\n");
+    printf("20---Kit Kat\n");
+    printf("21---5star\n");
+    printf("22---Perk\n");
+    printf("23---Munch\n");
+    printf("24---Milkybar\n");
+    printf("25---Snickers\n");
+    printf("26---Cavin's Milkshake(Choclate)\n");
+    printf("27---Cavin's Milkshake(Strawberry)\n");
+    printf("28---Cavin's Milkshake(Vanilla)\n");
+    printf("29---Heritage(Badam flavoured Milk)\n");
+    printf("30---Jersy(Badam flavoured Milk)\n");
+    printf("enter your choice\n");
+    scanf("%f",&choice);
+    switch(choice)
+        {
+            case 1:
+                {
+                    printf("You choose Lays\n");
+                    cost=25;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 2:
+                {
+                    printf("You choose Kurkure\n");
+                    cost =25;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 3:
+                {
+                    printf("You choose Pepsi\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 4:
+                {
+                    printf("You choose Thums Up\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 5:
+                {
+                    printf("You choose Sprite\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 6:
+                {
+                    printf("You choose Mirinda\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 7:
+                {
+                    printf("You choose Mountain Dew\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 8:
+                {
+                    printf("You choose Maaza\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 9:
+                {
+                    printf("You choose Slice\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 10:
+                {
+                    printf("You choose Lipton Green Tea\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 11:
+                {
+                    printf("You choose Bisleri\n");
+                    cost=20;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 12:
+                {
+                    printf("You choose Kinley\n");
+                    cost=20;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 13:
+                {
+                    printf("You choose Bailey\n");
+                    cost=20;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 14:
+                {
+                    printf("You choose Tropicana(Mango)\n");
+                    cost=15;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 15:
+                {
+                    printf("You choose Tropicana(Mixed fruit)\n");
+                    cost=15;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 16:
+                {
+                    printf("You choose Tropicana(Apple)\n");
+                    cost=15;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 17:
+                {
+                    printf("You choose Tropicana(Orange)\n");
+                    cost=15;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 18:
+                {
+                    printf("You choose Dairy milk\n");
+                    cost=20;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 19:
+                {
+                    printf("You choose Mars\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 20:
+                {
+                    printf("You choose Kit Kat\n");
+                    cost=20;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 21:
+                {
+                    printf("You choose 5star\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 22:
+                {
+                    printf("You choose Perk\n");
+                    cost=10;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 23:
+                {
+                    printf("You choose Munch\n");
+                    cost=10;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 24:
+                {
+                    printf("You choose Milkybar\n");
+                    cost=10;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 25:
+                {
+                printf("You choose Snickers\n");
+                cost=35;
+                printf("cost of the item is %f/-\n",cost);
+                printf("Please insert the amount to be paid\n");
+                break;
+                }
+            case 26:
+                {
+                    printf("You choose Cavin's Milkshake(Choclate)\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 27:
+                {
+                    printf("You choose Cavin's Milkshake(Strawberry)\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 28:
+                {
+                    printf("You choose Cavin's Milkshake(Vanilla)\n");
+                    cost=35;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 29:
+                {
+                    printf("You choose Heritage(Badam flavoured Milk)\n");
+                   cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            case 30:
+                {
+                    printf("You choose Jersy(Badam flavoured Milk)\n");
+                    cost=30;
+                    printf("cost of the item is %f/-\n",cost);
+                    printf("Please insert the amount to be paid\n");
+                    break;
+                }
+            default:
+            printf("Invalid Choice");
+            return (0);
+        }
+		printf("Enter the amount");
+        scanf("%f",&amount);
+        if(cost==amount)
+        {
+        printf("Enjoy your purchase");
+        }
+        else if (amount>cost)
+        {
+        change=amount-cost;
+        printf("Change you recieve is %f/-\n",change);
+        printf("Enjoy your purchase");
+        }
+        else if (amount<cost)
+        {
+        printf("You have entered the insufficient amount please re-enter the correct amount\n");
+        scanf("%f",&correctamount);
+        if(cost==correctamount) 
+        {
+            printf("Enjoy your purchase.");
+        }
+		else if(correctamount>cost)
+        {
+		change=correctamount-cost;
+		printf("change you recieve is %f/-\n",change);
+		printf("Enjoy your purchase");
+	    }
+	    else
+	    printf("We cannot dispence your product due to insufficient amount ");
+	    }
+	    else
+	    return(0);
+}
